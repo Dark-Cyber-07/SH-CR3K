d15b6c86aff491599a71017d210a696ef88bd713
MD SHANTO HASAN <85592998+Dark-Cyber-07@users.noreply.github.com>
2024-03-16 21:30:40 +0600

    CR3K.py

--- CR3K.py
+++ CR3K.py
@@ -1,1 +1,31 @@
-CR3K
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from XRF64_XF import Main
+ 
+        Main()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from XRF32_XF import Main
+ 
+ 
+        Main()
+ 
